2bb7f863914f1ed12a4bced27c7085944eebf7d0
Schemetrical <Schemetrical@icloud.com>
2018-05-13 13:04:29 -0700

    Fix timetable, add template for adding blocks, course tracking

--- parser.js
+++ parser.js
@@ -22,48 +22,65 @@ function parseSections(year, session, subject, course, term, completion) {
         var rows = doc.find('.section-summary')[0].children[1].children
         
         for (let row of rows) {
-            let items = row.children
-            let status = $(items[0]).text()
-            let section = $(items[1]).text()
-            let activity = $(items[2]).text()
-            let courseTerm = $(items[3]).text()
-            // let interval = $(items[4]).text()
-            let days = $(items[5]).text()
-            let startTime = $(items[6]).text()
-            let endTime = $(items[7]).text()
-            // let comments = $(items[8]).text()
-            if (courseTerm !== term && courseTerm !== "1-2") {
-                sections.push({
-                    status: status, 
-                    section: section, 
-                    activity: activity, 
-                    times: []
-                })
-                continue // Ignore terms that do not apply but take note
-            }
-            if (section === "") {
-                sections[sections.length - 1].times.push({
-                    days: days, 
-                    startTime: startTime, 
-                    endTime: endTime
-                })
+            parseRow(row, sections)
+        }
+        completion(postprocessSections(sections))
+    }
+
+    function parseRow(row, sections) {
+        let items = row.children
+        let status = $(items[0]).text()
+        let section = $(items[1]).text()
+        let activity = $(items[2]).text()
+        let courseTerm = $(items[3]).text()
+        // let interval = $(items[4]).text()
+        let days = $(items[5]).text()
+        let startTime = $(items[6]).text()
+        let endTime = $(items[7]).text()
+        // let comments = $(items[8]).text()
+        if (courseTerm !== term && courseTerm !== "1-2") {
+            sections.push({status: status, section: section, activity: activity, times: []})
+            return // Ignore terms that do not apply but take note
+        }
+        if (section === "") {
+            sections[sections.length - 1].times.push({
+                days: days, 
+                startTime: startTime, 
+                endTime: endTime
+            })
+            return
+        }
+        sections.push({status: status, section: section, activity: activity, times: [{
+                days: days, 
+                startTime: startTime, 
+                endTime: endTime
+            }]
+        })
+    }
+
+    function postprocessSections(sections) {
+        sections = sections.filter(function(section) {
+            // filter out all sections with no times or waitlist
+            return section.times.length > 0 && section.activity !== "Waiting List" && $.trim(section.times[0].days) !== ""
+        })
+        if (sections.length <= 0) return
+        // take the first item's activity as the main activity, such as "Lecture" or "Laboratory"
+        let mainActivity = sections[0].activity
+        newSections = []
+        for (section of sections) {
+            if (section.activity === mainActivity) {
+                section.subactivities = {}
+                newSections.push(section)
                 continue
             }
-            sections.push({
-                status: status, 
-                section: section, 
-                activity: activity, 
-                times: [{
-                    days: days, 
-                    startTime: startTime, 
-                    endTime: endTime
-                }]
-            })
+            var subactivities = newSections[newSections.length - 1].subactivities
+            if (!(section.activity in subactivities)) {
+                subactivities[section.activity] = []
+            }
+            subactivities[section.activity].push(section)
         }
-        sections = sections.filter(function(section) {
-            return section.times.length > 0
-        })
-        completion(sections)
+        return newSections
     }
+
     $.ajax({ url: `https://cors-anywhere.herokuapp.com/https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=3&sessyr=${year}&sesscd=${session}&dept=${subject}&course=${course}`, success: parse });
 }
--- scheduler.js
+++ scheduler.js
@@ -1,8 +1,20 @@
 window.onload = setup
+var LocalTime = JSJoda.LocalTime;
+var courses = {}
+
+$('#coursesTable').on('click','.delete',function(){
+    $(this).parents('tr').remove();
+    delete courses[$(this).parents('tr').children('th').text()]
+});
 
 function setup() {
     loadSessions()
     loadEmptyTimetable()
+    debugSetup()
+}
+
+function debugSetup() {
+    addCourseToTable("CPSC 121", [])
 }
 
 function loadSessions() {
@@ -17,9 +29,11 @@ function loadSessions() {
 
 function loadEmptyTimetable() {
     var timetable = $("#timetable > tbody")
-    for (let i = 1000; i <= 2100; i += 30) {
+    let endTime = LocalTime.parse('21:00')
+    let formatter = JSJoda.DateTimeFormatter.ofPattern('kk:mm')
+    for (let i = LocalTime.parse('08:00'); !i.isAfter(endTime); i = i.plusMinutes(30)) {
         row = $("<tr></tr>")
-        row.append($(`<th scope=\"row\">${i}</th>`))
+        row.append($(`<th scope=\"row\">${i.format(formatter)}</th>`))
         for (let j = 0; j < 5; j++) {
             row.append($("<td></td>"))
         }
@@ -28,14 +42,42 @@ function loadEmptyTimetable() {
 }
 
 function addCourse() {
-    let yearSession = $("#inputSession").val()
-    let year = yearSession.slice(0, -1)
-    let session = yearSession.substr(-1);
+    let yearSession = $("#inputSession").val() // 2017W
     let subject = $("#inputSubject").val()
     let course = $("#inputCourse").val()
     let term = $("#inputTerm").val()
-    if (course == "" || subject == "") {
-        return
-    }
-    parseSections(year, session, subject, course, term, function(sections) { console.log(sections) })
+    let courseName = subject + " " + course
+
+    // TODO: Need error handling here
+    if (course === "" || subject === "" || yearSession == null) return
+    if (courseName in courses) return
+
+    let year = yearSession.slice(0, -1) // 2017
+    let session = yearSession.substr(-1); // W
+    $("#buttonAdd").attr("disabled", true);
+    lockSectionAndTerm()
+    parseSections(year, session, subject, course, term, function(sections) { 
+        $("#buttonAdd").attr("disabled", false);
+        addCourseToTable(courseName, sections)
+        console.log(sections) 
+    })
+}
+
+function lockSectionAndTerm() {
+    $("#inputSession").attr("disabled", true);
+    $("#inputTerm").attr("disabled", true);
+}
+
+function addCourseToTable(courseName, sections) {
+    courses[courseName] = sections
+    var courseTable = $("#coursesTable > tbody")
+    row = $("<tr></tr>")
+    // TODO: Change this to dropdown allowing locking course sections
+    row.append($(`<th scope=\"row\">${courseName}</th>`))
+    row.append($("<td><button type=\"submit\" class=\"btn btn-danger btn-sm delete\">Remove</button></td>"))
+    courseTable.append(row)
+}
+
+function addEmptyBlock() {
+    
 }
