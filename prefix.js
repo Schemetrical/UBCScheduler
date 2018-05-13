fab638ef15fe83fbd85978170a55d887ffcdd6d4
Schemetrical <Schemetrical@icloud.com>
2018-05-13 15:39:46 -0700

    rename, include prefix

---
+++ prefix.js
@@ -0,0 +1,9 @@
+var LocalTime = JSJoda.LocalTime;
+var Weekday = {
+    None      : 0,       // 0 *
+    Monday    : 1 << 0,  // 1
+    Tuesday   : 1 << 1,  // 2
+    Wednesday : 1 << 2,  // 4
+    Thursday  : 1 << 3,  // 8
+    Friday    : 1 << 4,  // 16
+};
